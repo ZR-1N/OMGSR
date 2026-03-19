998c50ea2fc05d737b600f407c2c362324b09858
ZR-1N <shangwenxuan.nku@gmail.com>
2026-03-19 23:16:45 +0800

    modify help description

--- mid_timestep/mid_timestep_sd.py
+++ mid_timestep/mid_timestep_sd.py
@@ -142,7 +142,7 @@ def main():
 def parse_args():
     parser = argparse.ArgumentParser(description="Find optimal timestep for diffusion model")
     
-    parser.add_argument("--dataset_txt_or_dir_paths", type=str, nargs='+', help="List of dataset paths or txt files containing paths")
+    parser.add_argument("--dataset_txt_or_dir_paths", type=str, nargs='+', help="List of dataset paths or txt files containing paths (multiple allowed)")
     parser.add_argument("--sd_path", default="stabilityai/stable-diffusion-2-1",
                        help="Path to sd model")
     parser.add_argument("--resolution", type=int, default=512, 
